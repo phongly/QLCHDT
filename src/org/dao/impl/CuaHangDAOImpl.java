3c4981fb43bf1164d9926534e4e229b3542dd787
lythephong <windboy.phong@gmail.com>
2015-05-05 16:35:38 +0700

    create and init DAOImpl files

---
+++ src/org/dao/impl/CuaHangDAOImpl.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.dao.impl;
+
+import java.sql.SQLException;
+import java.util.List;
+import org.dao.api.CuaHangDAO;
+import org.pojo.CuaHang;
+/**
+ *
+ * @author ly.phong
+ */
+public class CuaHangDAOImpl implements CuaHangDAO{
+
+    @Override
+    public List<CuaHang> getAllCuaHang() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public CuaHang getCuaHangByID(int id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteCuaHang(CuaHang cuaHang) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteCuaHang(int id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void insertCuaHang(CuaHang cuaHang) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateCuaHang(CuaHang cuaHang) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
